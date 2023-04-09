d0f55c1a5c3b184af1a5db3dfa0f29bcc82b36b3
Hjun1004 <hjun1004@naver.com>
2023-04-09 23:34:55 +0900

    인스타 인증 회원만 본인의 인기지í표를 확인하도록 (ToString 예외도 작성)

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/entity/LikeablePerson.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/entity/LikeablePerson.java
@@ -28,9 +28,11 @@ public class LikeablePerson {
     private LocalDateTime modifyDate;
 
     @ManyToOne
+    @ToString.Exclude //양방향 관계에 대해서 출력에 문제가 발생할 수 있기 때문에 작성해주었다.
     private InstaMember fromInstaMember; // 호감을 표시한 사람(인스타 멤버)
     private String fromInstaMemberUsername; // 혹시 몰라서 기록
     @ManyToOne
+    @ToString.Exclude //양방향 관계에 대해서 출력에 문제가 발생할 수 있기 때문에 작성해주었다.
     private InstaMember toInstaMember; // 호감을 받은 사람(인스타 멤버)
     private String toInstaMemberUsername; // 혹시 몰라서 기록
     private int attractiveTypeCode; // 매력포인트(1=외모, 2=성격, 3=능력)
